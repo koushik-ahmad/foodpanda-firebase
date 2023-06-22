434d87eb058a032e0c65bade1ccd03744b10a0e2
koushik-ahmad <koushikahmad123@gmail.com>
2023-06-22 10:40:10 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBRCDhPLXf7weWQ5FDHoU-yZ-D9FRlp3eo",
+    authDomain: "foodpanda-firebase-e7228.firebaseapp.com",
+    projectId: "foodpanda-firebase-e7228",
+    storageBucket: "foodpanda-firebase-e7228.appspot.com",
+    messagingSenderId: "619460004715",
+    appId: "1:619460004715:web:72b43c29f082890e43da2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
